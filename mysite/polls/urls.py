25707573749eb25742427322c500500eef857e8c
Eric <nextstep@ericdee.me>
2022-01-16 20:12:33 -0700

    Moving on to part 4

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,19 @@ from django.urls import path
 
 from . import views
 
+# By setting the app name, url references are not confused between apps with the same naming conventions
+app_name = 'polls'
+
 urlpatterns = [
+    ###
+#   Patterns for /polls/
+    ###
     path('', views.index, name='index'),
+    path('<int:render_method_num>/', views.index, name='index'),
+    # EG /polls/5:
+    path('question/<int:question_id>/', views.detail, name='detail'),
+    # EG /polls/5/results/:
+    path('question/<int:question_id>/results/', views.results, name='results'),
+    # EG /polls/5/vote/:
+    path('question/<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django.http import Http404
 
-def index(request):
-    return HttpResponse('Hello, world. You\'re at the polls index.')
+def index(request, render_method_num=0):
+    match render_method_num:
+
+        case 0:
+            # The full way to load a template
+            # This makes it eaiser to read/translate what the render sequence is actually doing.
+            latest_question_list = Question.objects.order_by('-pub_date')[:5]
+            template = loader.get_template('polls/index.html')
+            context = {
+                'latest_question_list' : latest_question_list,
+            }
+            return HttpResponse(template.render(context, request))
+
+        case 1:
+            return _load_index_(request)
+
+        case 2:
+            # The shortcut for loading a template
+            # The template assignment isn't needed. Just use the render method, the request, the path, and the context.
+            latest_question_list = Question.objects.order_by('-pub_date')[:5]
+            context = {
+                'latest_question_list' : latest_question_list,
+            }
+            return render(request, 'polls/index.html', context)
+
+        case _:
+            raise Http404('That page has not been made yet.')       
+
+def detail(request, question_id):
+    match question_id:
+
+        case 0:
+            try:
+                question = Question.objects.get(pk=question_id)
+            except:
+                raise Http404('That question hasn\'t been asked yet.')
+            return HttpResponse('You\'re looking at question %s.' % question_id)
+        
+        case _:
+            question = get_object_or_404(Question, pk=question_id)
+            template = loader.get_template('polls/detail.html')
+            context = {
+                'route_message' : 'The default case ran.',
+                'question' : question
+            }
+            return HttpResponse(template.render(context, request))
+
+def results(request, question_id):
+    response = 'You\'re looking at question %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('You\'re voting on question %s.' % question_id)
+
+def _load_index_(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
